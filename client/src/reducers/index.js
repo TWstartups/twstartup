0e19c34a85b93f4dacf2da9c36928782164263ee
amazingshellyyy <amazingshellyyy@gmail.com>
2020-05-29 00:01:48 -0700

    reduxform reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
   user: userReducer,
+  form: formReducer
 })
+
