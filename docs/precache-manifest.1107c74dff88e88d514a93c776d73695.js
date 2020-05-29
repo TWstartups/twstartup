d763f8321d13a387947b25ffdbd394b6cfa7bd91
amazingshellyyy <amazingshellyyy@gmail.com>
2020-05-29 00:34:03 -0700

    create docs and ready to deploy

---
+++ docs/precache-manifest.1107c74dff88e88d514a93c776d73695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6457e3e490b3fcfdef83a068f77f67e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "540b7502f3dee8a737d3",
+    "url": "./static/css/main.ce004f84.chunk.css"
+  },
+  {
+    "revision": "e672e24c1eaae93a9b56",
+    "url": "./static/js/2.b05e0567.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b05e0567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "540b7502f3dee8a737d3",
+    "url": "./static/js/main.6386dcba.chunk.js"
+  },
+  {
+    "revision": "f35dbb30bb93cebd5c82",
+    "url": "./static/js/runtime-main.f63e6028.js"
+  },
+  {
+    "revision": "fd8739792865bb7a34bb65c6ea9c774e",
+    "url": "./static/media/logo.fd873979.svg"
+  }
+]);
