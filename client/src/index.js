ee52d2c4f9c2a9d8abc00c71fe2ceb10a53dcbf7
amazingshellyyy <amazingshellyyy@gmail.com>
2020-05-29 00:00:30 -0700

    wire up middleware and redux dev tool

--- client/src/index.js
+++ client/src/index.js
@@ -5,10 +5,13 @@ import "./assets/scss/styles.scss";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { createStore, applyMiddleware} from "redux";
+import { createStore, applyMiddleware, compose} from "redux";
 import reducers from "./reducers";
 import history from './history';
-const store = createStore(reducers, applyMiddleware(thunk));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
