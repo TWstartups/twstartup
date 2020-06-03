01ea13032e18603ac3796e657e6bba973f114ff2
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-02 20:18:25 -0700

    check token if not there , redirect to login

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,6 +3,8 @@ import user from '../apis/user';
 import candidate from '../apis/candidate'
 import history from '../history';
 
+const token = localStorage.getItem('token');
+
 export const signUp = formValues => async dispatch => {
   //redux thunk will take the returned function and invoke it, after that, it will pass it back to the dispatch and go through reduc thunk again. this time, the disptch is return a plain opbject which redux thunk will not do anything but pass it through.
   try {
@@ -14,6 +16,7 @@ export const signUp = formValues => async dispatch => {
     //implement programmatic navigation
     history.push('/');
   } catch (err) {
+
     dispatch({
       type: SIGN_UP,
       payload: {err:err.response.data.message}
@@ -53,18 +56,25 @@ export const errMsgReset = () => {
   }
 }
 
-export const fetchUser = token => async dispatch =>{
-  console.log(token);
-  const response = await user.get(`./user/${token}`);
-  dispatch({
-    type: FETCH_USER,
-    payload: response.data
-  })
+export const fetchUser = () => async dispatch =>{
+  try {
+    const response = await user.get(`./profile`);
+    dispatch({
+      type: FETCH_USER,
+      payload: response.data
+    })
+  } catch(err) {
+    console.log(err.response.data.message)
+  }
+  
 }
 
 export const createCandi = formValues => async dispatch => {
+  if (!token) {
+    history.push('/login')
+  }
   try {
-    const response = await candidate.post('',{formValues});
+    const response = await candidate.post('/create',{formValues});
 
     dispatch({
       type: CREATE_CANDI,
@@ -73,6 +83,9 @@ export const createCandi = formValues => async dispatch => {
     history.push('/apply/success');
 
   } catch (err) {
+    if (err.response.status === 403) {
+      window.location = '#/login';
+    }
     dispatch({
       type: CREATE_CANDI,
       payload: {err:err.response.data.message}
@@ -81,15 +94,21 @@ export const createCandi = formValues => async dispatch => {
 }
 
 export const fetchCandi = (candidateId) => async dispatch => {
-  const token = localStorage.getItem('token');
+  console.log('candiId',candidateId)
+  if (!token) {
+    history.push('/login')
+  }
   try {
-    const response = await candidate.get(`/${candidateId}/${token}`);
+    const response = await candidate.get(`/${candidateId}`);
     dispatch({
       type: FETCH_CANDI,
       payload: response.data
     })
     
   } catch(err) {
+    if (err.response.status === 403) {
+      window.location = '#/login';
+    }
     dispatch({
       type: FETCH_CANDI,
       payload: {err:err.response.data.message}
@@ -99,16 +118,22 @@ export const fetchCandi = (candidateId) => async dispatch => {
 }
 
 export const fetchCandis = () => async dispatch => {
+  if (!token) {
+    history.push('/login')
+  }
   try {
-    const token = localStorage.getItem('token');
-    const response = await candidate.get(`/all/${token}`);
+    console.log('fetchCandis')
+    const response = await candidate.get(`/all`);
     console.log(response);
     dispatch({
       type: FETCH_CANDIS,
       payload: response.data
     })
   } catch (err) {
-    
+    if (err.response.status === 403) {
+      window.location = '#/login';
+    }
+    console.log(err.response.data.message)
     dispatch({
       type: FETCH_CANDIS,
       payload: {err:err.response.data.message}
