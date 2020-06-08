9395d6f430e52d576bc3d98a2bc4b8d811cdd288
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-07 20:52:51 -0700

    see application page

--- client/src/components/ShowApplication.js
+++ client/src/components/ShowApplication.js
@@ -1,11 +1,81 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchCandi, editCandi } from "../actions";
+import CandidateForm from "./CandidateForm";
+import _ from "lodash";
+import history from "../history";
 
 class ShowApplication extends React.Component {
-  render(){
-    return (
-      <div>show Application</div>
+  componentDidMount() {
+    this.props.fetchCandi(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    console.log(formValues);
+
+    this.props.editCandi(this.props.match.params.id, formValues);
+  };
+
+  renderStatus = () => {
+    const status= this.props.candidate.approve_status
+    if (status === true) {
+      return (
+        <div className="ui green label">Approved</div>
       )
+    } else {
+      return <div className="ui yellow label">Pending</div>
     }
   }
 
-export default ShowApplication;
+  render() {
+    if (!this.props.candidate) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <div className="show-application">
+        <div className="ui grid container">
+          <div className="ui vertical segment" style={{ width: "100%" }}>
+            <h2 className="uileft floated header">Your Application</h2>
+            <div>
+            {this.props.candidate && this.renderStatus()}
+            </div>
+            <div className="ui clearing divider"></div>
+            <div className="ui grid">
+              <div className="one wide column"></div>
+              <div className="five wide column">
+                <div className="ui small header">Company Name in English</div>
+                <div>{this.props.candidate.company_name_en}</div>
+              </div>
+              <div className="five wide column">
+                <div className="ui small header">Company Name in Chinese</div>
+                <div>{this.props.candidate.company_name_chi}</div>
+              </div>
+              <div className="five wide column">
+              <div className="ui small header">Company Contact Email</div>
+                <div>{this.props.candidate.company_email}</div></div>
+              <div className="one wide column"></div>
+              <div className="five wide column">
+              <div className="ui small header">Company Website</div>
+                <div>{this.props.candidate.website}</div></div>
+              <div className="five wide column">
+              <div className="ui small header">News/Media</div>
+                <div>{this.props.candidate.news}</div></div>
+              <div className="five wide column">
+              <div className="ui small header">Other support resource</div>
+                <div>{this.props.candidate.other}</div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ user, candidate, form }) => {
+  return { candidateId: user.candidate, candidate: candidate.candidate };
+};
+
+export default connect(mapStateToProps, { fetchCandi, editCandi })(
+  ShowApplication
+);
