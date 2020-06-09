e648da1be4349b2a7e8af894e2562310e43c0e62
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-09 16:24:45 -0700

    checkAuth HOC

---
+++ client/src/middlewares/checkAuth.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Login from '../components/Login';
+
+
+class AuthComponent extends React.Component {
+    isLogIn(){
+      return this.props.isLogIn;
+    }
+    render(){
+      return (
+        <div>
+          {this.isLogIn === true ? <Component {...this.props} />:<Login/>}
+        </div>
+      )
+    }
+}
+
+
+
+export default AuthComponent;
