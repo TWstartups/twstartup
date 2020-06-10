76b6e5fb8698ac7196d5c3dde07d6582ba14a6dd
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-09 22:46:11 -0700

    update admin middleware for token checking

--- server/middlewares.js
+++ server/middlewares.js
@@ -8,7 +8,7 @@ export default {
     } else {
       console.log('mdw')
       const token = req.headers.authorization.split(' ')[1];
-      if (token === null) {
+      if (token == null) {
         res.status(403).json({ message:'Please log in!'});
       }
       console.log('token',token)
@@ -34,25 +34,30 @@ export default {
     console.log('inadmin')
     if (!req.headers['authorization']) {
       res.status(403).json({ message:'Please log in!'});
-    } else {
+    }
       const token = req.headers.authorization.split(' ')[1];
-      if (token.length == 0) {
+      if (token == null) {
         res.status(403).json({ message:'Please log in!'});
       }
       console.log('token',token)
-      const user_id = await JWT.verifyToken(token);
-      const foundUser = await User.findById(user_id);
-      console.log(foundUser);
-      if (foundUser.type === 'super') {
-        console.log('yay it is admin')
-        req.user = foundUser;
-        console.log('after yay')
-        next();
-      } else {
-        
-       res.status(403).json({message:'You are not an admin'})
+      try {
+        const user_id = await JWT.verifyToken(token);
+        const foundUser = await User.findById(user_id);
+        console.log(foundUser);
+        if (foundUser.type === 'super') {
+          console.log('yay it is admin')
+          req.user = foundUser;
+          console.log('after yay')
+          next();
+        } else {
+          
+         res.status(403).json({message:'You are not an admin'})
+        }
+      } catch(err) {
+        res.status(500).json({message:'Something went wrong'})
       }
-    }
+      
+    
    
   }
 };
