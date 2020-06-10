e12563739e519c004cbe62914323182d9c8abffb
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-09 23:30:18 -0700

    show all and show one

--- server/company/controller.js
+++ server/company/controller.js
@@ -8,10 +8,20 @@ export default {
     res.status(200);
   },
   show: async (req, res) => {
-    res.status(200);
+    try {
+      const foundCompany = await Company.findById(req.params.id);
+      res.status(200).json({company:foundCompany});
+    } catch(err) {
+      res.status(200);
+    }
   },
   showAll: async (req, res) => {
-    res.status(200).json({message: 'get all company'});
+    try{
+      const allCompany = await Company.find();
+      res.status(200).json({companies:allCompany});
+    } catch (err) {
+      res.status(500).json({message:"something went wrong when getting all company data"});
+    }
   }
  
 }
