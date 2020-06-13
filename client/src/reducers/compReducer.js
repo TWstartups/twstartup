17ed73bb6126dd1ae01ff25f8c04c4e89b141160
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-13 13:27:34 -0700

    edit comp reducer

--- client/src/reducers/compReducer.js
+++ client/src/reducers/compReducer.js
@@ -1,4 +1,4 @@
-import { FETCH_COMPS, FETCH_COMP } from '../actions/types';
+import { FETCH_COMPS, FETCH_COMP, EDIT_COMP } from '../actions/types';
 
 const INITIAL_STATE = {
   errMsg: null,
@@ -14,6 +14,11 @@ export default (state = INITIAL_STATE, action) => {
       }
       return {...state, companylist: action.payload.companies}
     case FETCH_COMP:
+      if (action.payload.err) {
+        return {...state, errMsg: action.payload.err}
+      }
+      return {...state, currentCompany: action.payload.company}
+    case EDIT_COMP: 
       if (action.payload.err) {
         return {...state, errMsg: action.payload.err}
       }
