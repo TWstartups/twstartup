bf8c824f86842da8fbfbf7a17f7fd84382cf897e
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-13 11:40:20 -0700

    profile redux form

---
+++ client/src/components/company/modals/ProfileForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Field, reduxForm } from "redux-form";
+
+
+class ProfileForm extends React.Component {
+
+  renderInput = ({ input, label, placeholder, meta, type }) => {
+    return (
+      <div className="required field">
+        <label>{label}</label>
+        <input {...input} placeholder={placeholder} autoComplete="off" type={type}/>
+      </div>
+    )
+  }
+
+  render(){
+    return (
+      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+      <Field
+      name="company_name_en"
+      component={this.renderInput}
+      label="Company Name in English"
+      placeholder=""
+      type="text"
+    />
+    <Field
+      name="company_email"
+      component={this.renderInput}
+      label="Contact Email"
+      placeholder=""
+      type="tel"
+    />
+    <Field
+      name="website"
+      component={this.renderInput}
+      label="Company Website"
+      placeholder=""
+      type="text"
+    />
+    <button className='ui button' type="submit">Submit</button>
+      </form>
+    )
+  }
+}
+
+const validate = (formsValues) => {
+  const errors = {};
+  return 
+}
+
+
+export default reduxForm({
+  form:"profileForm",
+  validate
+})(ProfileForm);
