9209bae5353974154e01d873c8831e875ce34793
amazingshellyyy <amazingshellyyy@gmail.com>
2020-06-14 01:11:09 -0700

    delete news and event route

--- server/api.js
+++ server/api.js
@@ -1,8 +1,7 @@
 
 import CompanyApi from './company/router';
 import CandidateApi from './candidate/router';
-import NewsApi from './news/router';
-import EventApi from './event/router';
+
 
 import express from 'express';
 const router = express.Router();
@@ -11,8 +10,7 @@ router.get('/', (req, res)=> res.json({message:"/api connected"}))
 
 router.use('/candidate', CandidateApi)
 router.use('/company', CompanyApi)
-router.use('/news', NewsApi)
-router.use('/event', EventApi)
+
 
 
 export default router;
