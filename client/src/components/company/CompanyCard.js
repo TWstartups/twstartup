58d3caa91908cc3a8ca16670762c4767c414b08e
Andy Chen <amazingandyyy@gmail.com>
2020-08-21 00:10:48 -0700

    Update CompanyCard.js

--- client/src/components/company/CompanyCard.js
+++ client/src/components/company/CompanyCard.js
@@ -11,27 +11,11 @@ const CompanyCard = (props) => {
         </div>
         <div className="body">
           <div className="companyNameEn">{company.companyNameEn}</div>
-          {/* <div className="companyNameChi">{company.companyNameChi}</div> */}
+          <div className="companyNameChi">{company.companyNameChi}</div>
           <div className="introduction">
             {company.introduction}
           </div>
         </div>
-        {/* <div className="extra content"> */}
-        {/*  <div className="ui blue labels"> */}
-        {/*    <div className="ui label"> */}/Users/andychen/projects/twstartups/client/src/components/company/imageZone/index.js
-        {/*  Happy */}
-        {/*    </div> */}
-        {/*    <div className="ui label"> */}
-        {/*  Smart */}
-        {/*    </div> */}
-        {/*    <div className="ui label"> */}
-        {/*  Insane */}
-        {/*    </div> */}
-        {/*    <div className="ui label"> */}
-        {/*  Exciting */}
-        {/*    </div> */}
-        {/*  </div> */}
-        {/* </div> */}
       </div>
     </div>
   )
